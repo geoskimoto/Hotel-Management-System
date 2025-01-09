ee4734b2cb42d9c085d36308913fb26c8dca4351
Nick Steele <ns1930@g.rit.edu>
2025-01-09 10:34:14 -0800

    added some CSC content

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*t5wynyhd=2udczig6#n&0337+m=ga!p=cglnd-+srqdpq4r2'
+# SECRET_KEY = 'django-insecure-m*t5wynyhd=2udczig6#n&0337+m=ga!p=cglnd-+srqdpq4r2'
+SECRET_KEY = 'sk_test_51QfPSFCxEEHcFgxKnELuJtcjqtnow2bdkgIbwdcfYtT8oLz0kWqsNj1AwS0DgYNBptEHfhrUGZQrazf0vOWIp5sT00OsrSKGCe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
